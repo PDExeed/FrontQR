2dcf9e1db25de546994879f2bd527b057e2cf91d
llegaa <elenagavrilova234@gmail.com>
2024-05-14 10:23:25 +0300

    исправлены ошибки

--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -5,10 +5,13 @@ import {RootState} from "../../store/store";
 import carInformation from "../../store/carInformationSlice";
 export function Price({price}:PriceInterface){
     const data = useSelector((s: RootState)=> s.carInformation)
+    const formatPrice = (price: number)=>{
+        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+    }
     return(
         <div className={s.container}>
             <div className={s.text}>стоимость</div>
-            <div className={s.price}><span className={s.priceNumber}>{data.price}</span> ₽</div>
+            <div className={s.price}><span className={s.priceNumber}>{formatPrice(data.price)}</span> ₽</div>
         </div>
     )
 }
