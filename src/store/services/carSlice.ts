209f98d124690765f69bb966902aed2a13a63ec7
Буянов Всеволод <mors@nemors.ru>
2024-05-20 11:59:08 +0300

    fix

--- src/store/services/carSlice.ts
+++ src/store/services/carSlice.ts
@@ -8,7 +8,7 @@ interface CarData {
 export const carAPI = createApi({
     reducerPath: 'carAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://92.53.70.162:8000',
+        baseUrl: 'https://exeed-mospolytech.ru',
     }),
     tagTypes: ['Car'],
     endpoints: (build) => ({
